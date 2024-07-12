3d5a609ac457e3a9cca5a024f8c18a2d32a6a7e9
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2024-07-12 22:53:11 +0530

    Add files via upload

---
+++ s25-jsxml-rules-media/src/App.js
@@ -0,0 +1,198 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="body">
+      <h1 id="heading">Kalki 2898AD</h1>
+      <br></br>
+      <br></br>
+
+      <br></br>
+      <br></br>
+      <img
+        className="image"
+        src="https://content.tupaki.com/en/feeds/2024/05/22/395714-kal.gif"
+      ></img>
+      <p className="paraGraph">
+        Kalki 2898 AD is a 2024 Indian Telugu-language epic science fiction film
+        directed by Nag Ashwin and produced by Vyjayanthi Movies. The film stars
+        Amitabh Bachchan, Kamal Haasan, Prabhas, Deepika Padukone and Disha
+        Patani. Inspired by Hindu scriptures, it is the first instalment in a
+        planned Kalki Cinematic Universe. Set in a post-apocalyptic world in the
+        year 2898 AD, the film follows a select group on a mission to save
+        Kalki, the unborn child of SUM-80, a lab subject. The film was
+        officially announced in February 2020 under the tentative title Prabhas
+        21, as it is the actor's 21st film in the lead role and was later
+        changed to working title Project K. Principal photography commenced a
+        year later in July 2021 due to COVID-19 pandemic. It was shot
+        sporadically in several legs over the next three years, and wrapped by
+        late-May 2024. The official title was announced in July 2023. The film
+        has music composed by Santhosh Narayanan, cinematography handled by
+        Serbian cinematographer Djordje Stojiljkovic and editing by Kotagiri
+        Venkateswara Rao. Made on a ₹600 crore (US$72 million) production
+        budget, it is the most expensive Indian film ever made. Kalki 2898 AD
+        was initially scheduled to release on 9 May 2024, but was postponed due
+        to unfinished production works. It was released worldwide on 27 June
+        2024 in standard, IMAX, 3D and 4DX formats, and set several box office
+        records for a Telugu and Indian film.
+      </p>
+      <hr></hr>
+      <iframe
+        className="trailer"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/-rTzyZZGJ84?si=McLtd-KAAki9E_xp"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <img
+        className="image2"
+        src="https://telugucinema.com/wp-content/uploads/cwv-webp-images/2024/06/kalki-amitabh-long.jpg.webp"
+      ></img>
+      <h3>Plot:--</h3>
+      <p className="paraTwo">
+        In 3102 BC, following the Kurukshetra War, Ashwatthama attempts to kill
+        Uttarā's unborn child by invoking the Brahmashirastra, a godly weapon of
+        unimaginable destruction. For committing the act, Krishna, the 8th
+        reincarnation of Vishnu, curses him to roam the earth as an immortal and
+        witness humanity's misery as a result of societal and moral
+        deterioration in the Kali Yuga. His divine gem is also taken from his
+        forehead, and his redemption is destined to occur through protecting the
+        mother of Vishnu's next reincarnation, Kalki, towards the end of Kali
+        Yuga. In 2898 AD, six thousand years after the events of the Kurukshetra
+        War, the desertified city of Kasi stands as the last known city in a
+        post-apocalyptic world dominated by Supreme Yaskin, a totalitarian
+        god-king. Yaskin rules from the "Complex", an
+        inverted-pyramidal-megastructure that hovers above the city, extracting
+        Earth's resources to sustain a gated paradise accessible only to those
+        who can pay 1 million "units". His authority is enforced by Commander
+        Manas, Counsellor Bani and the Raiders, who vigilantly guard against
+        threats, particularly the prophesied arrival of Kalki. In this dystopian
+        society, fertile female refugees in Kasi are forcibly taken to the
+        Complex for Project K, an experiment aimed at producing a life-extending
+        serum for the 200-year-old Yaskin through artificially inseminated
+        foetuses. However, no woman survives more than 120 days of pregnancy,
+        resulting in their deaths. Raia, a young girl disguised as a boy,
+        narrowly escapes conscription with the help of rebels led by Rumi from
+        the hidden city of Shambhala. Rumi and Divya, a pregnant woman believed
+        to be carrying Kalki, are captured and publicly executed by Manas for
+        refusing to reveal Shambhala's location. After Manas sets a bounty on
+        Shambhala rebels, Raia finds refuge in a cave where she encounters
+        Ashwatthama. She unknowingly brings him his divine gem, Shivamani, which
+        has been found by and passed on among the Shambhala rebels. Together,
+        they set out to find the real mother of Kalki. SUM-80, a Project K
+        subject hiding her 150-day pregnancy, is eventually ousted and captured.
+        Manas extracts a single drop of serum from her foetus before the
+        procedure is interrupted by Lilly, a Shambhala rebel posing as a Project
+        K subject. Lilly helps SUM-80 escape the lab, but stays behind to hold
+        off the Raiders. SUM-80 is received by Veeran, Kyra, and Ajju, Shambhala
+        rebels contacted by Lilly. Kyra renames SUM-80 as "Sumathi". En route to
+        Shambhala, their convoy is ambushed by bounty hunters and Raiders after
+        Manas places a bounty on Sumathi. Though they fend off the attackers,
+        Kyra is killed. Bhairava, a bounty hunter aiming to secure Sumathi's
+        bounty to enter the Complex, remains in pursuit with his AI supercar,
+        Bujji. Ashwatthama learns through his gem that Sumathi's unborn child is
+        Kalki. Despite Bhairava's efforts, Ashwatthama subdues him and the party
+        reaches Shambhala, where the tree of life blooms in Kalki's presence.
+        Bhairava makes a deal with Manas to capture Sumathi in exchange for his
+        entry into the Complex. Bhairava discovers Shambhala by deceiving a
+        rebel, Luke, and disguises himself as Ashwatthama by using his hologram
+        technology. He convinces Sumathi to flee Shambhala, but they are
+        intercepted by the real Ashwatthama. As they fight, Shambhala's leader,
+        Mariam, attempts to take Sumathi away to safety. Manas and the Raiders
+        track Bhairava to Shambhala and breach its defences with a beam weapon.
+        The Raiders overwhelm the Shambhalan army, and Mariam is killed after
+        slashing Manas' face. He then chains Ashwatthama, whose walking stick
+        falls into the unconscious Bhairava's hands, where it is actually
+        revealed to be Karna's bow, the Vijaya Dhanush. Bhairava wield Karna bow
+        and gain its power, possibly hinting him as descendant of Karna.
+        Bhairava effortlessly defends Sumathi and Ashwatthama against the
+        Raiders. Bhairava uses the bow to rescue Sumathi and kill Manas.
+        Ashwatthama, recognising his former friend and brother-in-arms, breaks
+        free, but Bhairava reverts to his former self due to a distraction
+        caused by Bujji and takes Sumathi away. Back at the Complex, Yaskin is
+        informed of Manas's failure and death by Counsellor Bani. He uses the
+        extracted serum drop to transform into a much younger superhuman. He
+        picks up Arjuna's bow, Gandiva, and declares to personally capture
+        Sumathi and her child to reshape the world
+      </p>
+      <h3>Movie details :--</h3>
+      <table>
+        <tr>
+          <th>Directed by</th>
+          <td> Nag Ashwin</td>
+        </tr>
+        <tr>
+          <th>Written by</th>
+          <td>Nag Ashwin</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Rutham Samar</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Sai Madhav Burra</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>B. S. Sarawagna Kumar</td>
+        </tr>
+        <tr>
+          <th>Produced by</th>
+          <td>C. Aswani Dutt</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Swapna Dutt</td>
+        </tr>
+
+        <tr>
+          <th></th>
+          <td>Priyanka Dutt</td>
+        </tr>
+        <tr>
+          <th>Starring</th>
+          <td>Amitabh Bachchan</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Kamal Haasan</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Prabhas</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Deepika Padukone</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>Disha Patani</td>
+        </tr>
+        <tr>
+          <th>Music by</th>
+          <td>Santhosh Narayanan</td>
+        </tr>
+        <tr>
+          <th>Production company</th>
+          <td>Vyjayanthi Movies</td>
+        </tr>
+        <tr>
+          <th>Budget</th>
+          <td>₹600 crore</td>
+        </tr>
+        <tr>
+          <th>Box office</th>
+          <td>₹1,000 crore +..</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
